796caf8cb853f24c6746d8f6e1714dae08940680
Sheeeeen <43342103+Sheeeeen@users.noreply.github.com>
2021-05-05 17:06:36 -0400

    finished dialogue screen

--- src/App.js
+++ src/App.js
@@ -4,29 +4,61 @@ import Dialogue from "./dialogue.json";
 
 export default function App() {
   const [progress, setProgress] = useState(0);
-  console.log(progress);
   return (
     <div className="App">
-      <DialogueScreen dialogueScene={Dialogue[progress]} />
+      <DialogueScreen dialogueScene={Dialogue[progress]} progress={progress} setProgress={setProgress}/>
     </div>
   );
 }
 
-// Component Name: dialogueScreen
+// Button Component
+// Prop Names: buttonText, buttonClick
+function Button(props) {
+    return (
+      <button onClick={props.buttonClick} index={props.index}>{props.buttonText}</button>
+    )
+}
+// Dialogue Box
+function Speech(props) {
+  return(
+    <p>{props.speechBubble}</p>
+  )
+}
 
-// Where will this component be located?
+// Avatar/Img
+function Portrait(props){
+  return(
+    <img src={props.characterArt} />
+  )
+}
+// Name Plate
+function Name(props){
+  return(
+    <h4>{props.characterName}</h4>
+  )
+}
 
-// Will this component use props? yes
-// Name of props: dialogue, characterInfo, screenChange
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 
-// Will this component use state? yes
-// Name of state variables: dialogueStep, dialogueSwitch
 
 function DialogueScreen(props) {
-  console.log(props);
+  function buttonClick(event) {
+    const selection = event.target.attributes.index.value;
+    // if i clicked death button
+      // console log im dead
+    // else
+     // return(selection===death : )
+      props.setProgress(props.progress+1)
+  }
+
   return (
-    <div className="name">
-      <h1> Camelot </h1>
+    <div className="dialogue-screen">
+      <Portrait characterArt={props.dialogueScene.character_art} />
+      <Name characterName={props.dialogueScene.name} />
+      <Speech speechBubble={props.dialogueScene.text} />
+      <div class="dialogue-options">
+        {props.dialogueScene.choices.map((choize, index) => <Button buttonText={choize} buttonClick={buttonClick} index={index} /> )}
+      </div>
     </div>
   );
 }
